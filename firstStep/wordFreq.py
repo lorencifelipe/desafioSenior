bd8ec0ca8c00a29049bd168ba6c5608ecf8c494f
Felipe Lorenci <lorencifelipe@gmail.com>
2022-06-06 16:38:52 -0300

    Ajustado header, para melhor leitura do código.

--- firstStep/wordFreq.py
+++ firstStep/wordFreq.py
@@ -1,7 +1,7 @@
-#Return a dictionary with frequencies to each word 
+# Return a dictionary with frequencies to each word
 def wordsFreq(data):
     wordFreq = {}
-    for header in data:
-        if(header != "Full_Text" and header != "Common_Word_Count" and header != "Word_Count" and header != "Date" and header != "IsSpam"):
-            wordFreq[header] = int(data[header].sum())
+    header = list(data.columns)
+    for col in header[1:-4]:
+        wordFreq[col] = int(data[col].sum())
     return(wordFreq)
