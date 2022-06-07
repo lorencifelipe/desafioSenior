593325444554039fd4ce41f13970a3f7435e5490
Felipe Lorenci <lorencifelipe@gmail.com>
2022-06-07 18:49:36 -0300

    Refatorada função daysLessSpam().

--- firstStep/lessSpam.py
+++ firstStep/lessSpam.py
@@ -1,24 +1,20 @@
 def daysLessSpam(data):
-    bestDay = {}
-    j = 0
-    for i in data["Date"]:
-        d = i[8:-9]  # day
-        m = i[5:-12]  # month
-        if(m not in bestDay):  # new month
+    bestDays = {}
+    months = data["Date"].dt.month.unique()
+    for m in months:
+        best = 0
+        days = data.query("Date.dt.month == @m")["Date"].dt.day.unique()
+        for d in days:
             counter = 0
-            best = 0
-            previous = int(d)
-            bestDay[m] = 0
-        if(data["IsSpam"][j] == "no" and d == previous):
-            counter += 1
-        elif(data["IsSpam"][j] == "no"):
-            counter = 1
-        else:
-            counter = 0
-        if(counter > best):
-            best = counter
-            bestDay[m] = d
-        previous = d
-        j += 1
-    for i in bestDay:
-        print("Mês " + str(i) + ": dia " + str(bestDay[i]) + "\n")
+            spamsDaily = data.query(
+                "Date.dt.month == @m & Date.dt.day == @d")["IsSpam"]
+            for mail in spamsDaily:
+                if(mail == "no"):
+                    counter += 1
+                else:
+                    counter = 0
+                if(counter > best):
+                    best = counter
+                    bestDays[m] = d
+    for i in bestDays:
+        print("Mês " + str(i) + ": dia " + str(bestDays[i]) + "\n")
